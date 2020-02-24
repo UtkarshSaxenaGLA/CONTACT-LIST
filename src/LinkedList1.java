8d39a10627cf13d9a991f2011cfae57f2b1e3bd7
UtkarshSaxenaGLA <49234966+UtkarshSaxenaGLA@users.noreply.github.com>
2020-02-24 09:07:07 -0800

    Create LinkedList1.java

---
+++ src/LinkedList1.java
@@ -0,0 +1,56 @@
+package bylinkedlist;
+
+public class LinkedList1 {
+    Person person;
+    private Node first;
+    private Node last;
+
+    public void addLast(Person person) {
+        Node node = new Node(person);
+        if (isEmpty()) {
+            first = node;
+            last = node;
+        } else {
+            last.setNext(node);
+            last = node;
+        }
+    }
+
+    public void addFirst(Person person) {
+        Node node = new Node(person);
+        if (isEmpty()) {
+            first = node;
+            last = node;
+        } else {
+            node.setNext(first);
+            first = node;
+        }
+    }
+
+    public boolean isEmpty() {
+        boolean response = false;
+        if (first == null) {
+            response = true;
+        }
+        return response;
+    }
+
+    public boolean exist(String firstName) {
+        if (indexOf(firstName) != -1) {
+            return true;
+        }
+        return false;
+    }
+
+    public int indexOf(String firstName) {
+        Node currently = first;
+        int response = 0;
+        while (currently != null) {
+            if (currently.getValue().getFirstName().equals(firstName)) {
+                return response;
+            }
+            response++;
+            currently = currently.getNext();
+        }
+        return -1;
+    }
